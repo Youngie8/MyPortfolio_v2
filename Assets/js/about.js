258678a2b25c2dc1bc737df0e852557abb82284f
taiwo oluwole-young <taiwooluwoleyoung@gmail.com>
2022-08-08 17:49:49 +0100

    removed the skills and about files

--- Assets/js/about.js
+++
@@ -1,5 +0,0 @@
-const section = document.querySelector('section')
-const body = document.querySelector("body");
-section.addEventListener("click", function(){
-    body.classList.toggle("changeTheme");
-});
